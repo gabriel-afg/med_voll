22afa0f8295fe4d74cf8879a2ecaebf3ccf41117
Gabriel Augusto <Gabriel.afg182@gmail.com>
2023-10-16 12:05:31 -0300

    Exception Controller

---
+++ src/main/java/md/voll/api/exceptions/ExceptionHandlerController.java
@@ -0,0 +1,28 @@
+package md.voll.api.exceptions;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlerController {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404(){
+        return ResponseEntity.notFound().build();
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException e){
+        var errors = e.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(errors.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem){
+        public DadosErroValidacao(FieldError e){
+            this(e.getField(), e.getDefaultMessage());
+        }
+    }
+}
